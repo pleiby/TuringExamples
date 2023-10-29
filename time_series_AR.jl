133c32ffbe03fb82bc1d6d421bbc0e03676c0421
Paul Leiby <pleiby@gmail.com>
2023-10-28 22:21:42 -0400

    restyle (auto) to julia default (mostly whitespace subleties)

--- time_series_AR.jl
+++ time_series_AR.jl
@@ -17,34 +17,34 @@ println("Loading the dataset")
 df = CSV.read("../data/shampoo.csv")
 s = df.Sales
 pyplot()
-plot(s, reuse = false, title = "Shampoo dataset")
+plot(s, reuse=false, title="Shampoo dataset")
 gui()
 
 println("Split into training and test sets. We will predict for the next 4 days using the data from the past 32 days")
 train_percentage = 0.9
-s_train = s[1:floor(Int, train_percentage*length(s))]
+s_train = s[1:floor(Int, train_percentage * length(s))]
 N = length(s_train)
 
 println("Plot the training data")
-plot(s_train, reuse = false, title = "Train Data")
+plot(s_train, reuse=false, title="Train Data")
 gui()
 
-println("Plotting ACF and PACF plots") 
-s1 = scatter([1, 2, 3, 4, 5], autocor(s, [1, 2, 3, 4, 5]), title = "ACF", ylim = [0.3,0.8])
-s2 = scatter([1, 2, 3, 4, 5], pacf(s, [1, 2, 3, 4, 5]), title = "PACF", ylim = [0.3,0.8])
-plot(s1, s2, layout = (2, 1), reuse = false)
+println("Plotting ACF and PACF plots")
+s1 = scatter([1, 2, 3, 4, 5], autocor(s, [1, 2, 3, 4, 5]), title="ACF", ylim=[0.3, 0.8])
+s2 = scatter([1, 2, 3, 4, 5], pacf(s, [1, 2, 3, 4, 5]), title="PACF", ylim=[0.3, 0.8])
+plot(s1, s2, layout=(2, 1), reuse=false)
 gui()
 println("The PACF plot cuts off at k = 2, so we will have an AR(2) model for this dataset")
 
 #Defining the model
 σ = 1.0
 @model AR(x, N) = begin
-    α ~ Normal(0,σ) 
+    α ~ Normal(0, σ)
     beta_1 ~ Uniform(-1, 1)
     beta_2 ~ Uniform(-1, 1)
     for t in 3:N
-        μ = α + beta_1 * x[t-1] + beta_2 * x[t - 2] 
-        x[t] ~ Normal(μ, 0.1) 
+        μ = α + beta_1 * x[t-1] + beta_2 * x[t-2]
+        x[t] ~ Normal(μ, 0.1)
     end
 end;
 
@@ -53,14 +53,14 @@ end;
 # n_adapts::Int : The number of samples to use with adapatation.
 # δ::Float64 : Target acceptance rate.
 
-chain = sample(AR(s_train, N), NUTS(5000, 200, 0.65) )
+chain = sample(AR(s_train, N), NUTS(5000, 200, 0.65))
 
 println("Chain has been sampled; Now let us visualise it!")
-plot(chain, reuse = false, title = "Sampler Plot")
+plot(chain, reuse=false, title="Sampler Plot")
 gui()
 
 #Plotting the corner plot for the chain
-corner(chain, reuse = false, title = "Corner Plot")
+corner(chain, reuse=false, title="Corner Plot")
 gui()
 
 println("Removing the warmup samples...")
@@ -73,22 +73,22 @@ beta_2 = mean(chains_new[:beta_2].value)
 
 
 println("Obtaining the test data")
-s_test = s[N + 1:length(s)]
+s_test = s[N+1:length(s)]
 
 println("Obtaining the predicted results using the mean values of beta_1 and beta_2")
 s_pred = Float64[]
-first_ele =  s_train[N]*beta_1 + s_train[N - 1]*beta_2 + rand(Normal(0,1))
+first_ele = s_train[N] * beta_1 + s_train[N-1] * beta_2 + rand(Normal(0, 1))
 push!(s_pred, first_ele)
-second_ele = s_pred[1]*beta_1 + s_train[N]*beta_2 + rand(Normal(0,1))
+second_ele = s_pred[1] * beta_1 + s_train[N] * beta_2 + rand(Normal(0, 1))
 push!(s_pred, second_ele)
-for i=3:length(s_test)
-    next_ele = s_pred[i - 1]*beta_1 + s_pred[i - 2]*beta_2 + rand(Normal(0,1))
+for i = 3:length(s_test)
+    next_ele = s_pred[i-1] * beta_1 + s_pred[i-2] * beta_2 + rand(Normal(0, 1))
     push!(s_pred, next_ele)
 end
 
 println("Plotting the test and the predicted data for comparison")
-plot(s_test, reuse = false, title = "Predicted vs Test Comparison", label = "Test")
-plot!(s_pred, label = "Predicted")
+plot(s_test, reuse=false, title="Predicted vs Test Comparison", label="Test")
+plot!(s_pred, label="Predicted")
 gui()
 
 println("Press ENTER to exit")
