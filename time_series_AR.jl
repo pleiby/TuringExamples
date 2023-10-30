fbec46edbfabb1f6dff8056ed6eb898d9adc3605
Paul Leiby <pleiby@gmail.com>
2023-10-30 15:33:23 -0400

    update syntax for mean calc; test & comment prediction (flawed currently)

--- time_series_AR.jl
+++ time_series_AR.jl
@@ -29,21 +29,24 @@ println("We will predict for the next $(length(s)-N) obs using the data from the
 
 println("Plot the training data")
 plot(s_train, reuse=false, title="Train Data")
-gui()
+# gui()
 
 println("Plotting ACF and PACF plots")
 s1 = scatter([1, 2, 3, 4, 5], autocor(s, [1, 2, 3, 4, 5]), title="ACF", ylim=[0.3, 0.8])
 s2 = scatter([1, 2, 3, 4, 5], pacf(s, [1, 2, 3, 4, 5]), title="PACF", ylim=[0.3, 0.8])
 plot(s1, s2, layout=(2, 1), reuse=false)
-gui()
+# gui()
 println("The PACF plot cuts off at k = 2, so we will have an AR(2) model for this dataset")
 
 #Defining the model
 σ = 1.0
 @model AR(x, N) = begin
+    # prior
     α ~ Normal(0, σ)
     beta_1 ~ Uniform(-1, 1)
     beta_2 ~ Uniform(-1, 1)
+    sigma ~ Exponential(1) # ??? Unused
+    # likelihood
     for t in 3:N
         μ = α + beta_1 * x[t-1] + beta_2 * x[t-2]
         x[t] ~ Normal(μ, 0.1)
@@ -63,25 +66,28 @@ chain = sample(model, sampler, samples)
 
 println("Chain has been sampled; Now let us visualise it!")
 plot(chain, reuse=false, title="Sampler Plot")
-gui()
+# gui()
 
 #Plotting the corner plot for the chain
 corner(chain, reuse=false, title="Corner Plot")
-gui()
+# gui()
 
 println("Removing the warmup samples...")
 chains_new = chain[50:4800]
 show(chains_new)
 
 # Getting the mean values of the sampled parameters
-beta_1 = mean(chains_new[:beta_1].value)
-beta_2 = mean(chains_new[:beta_2].value)
+#v beta_1 = mean(chains_new[:beta_1].value) # returns ERROR: type AxisArray has no field valeu
+beta_1 = mean(chains_new[:beta_1][:, 1]) # returns ERROR: type AxisArray has no field valeu
+beta_2 = mean(chains_new[:beta_2][:, 1])
 
 
 println("Obtaining the test data")
 s_test = s[N+1:length(s)]
 
 println("Obtaining the predicted results using the mean values of beta_1 and beta_2")
+# ??? Three strange things here:
+#  prediction using lagged actual (vs fitted) values; exog normal disturbance term; dropped α from model
 s_pred = Float64[]
 first_ele = s_train[N] * beta_1 + s_train[N-1] * beta_2 + rand(Normal(0, 1))
 push!(s_pred, first_ele)
@@ -93,9 +99,9 @@ for i = 3:length(s_test)
 end
 
 println("Plotting the test and the predicted data for comparison")
-plot(s_test, reuse=false, title="Predicted vs Test Comparison", label="Test")
-plot!(s_pred, label="Predicted")
-gui()
+p_ests = plot(s_test, reuse=false, title="Predicted vs Test Comparison", label="Test")
+plot!(p_ests, s_pred, label="Predicted")
+# gui()
 
-println("Press ENTER to exit")
-read(stdin, Char)
+# println("Press ENTER to exit")
+# read(stdin, Char)
